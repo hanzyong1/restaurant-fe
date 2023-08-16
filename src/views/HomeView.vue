8509bc01a65ee28d225f7b3823fc7fc194263b87
Han <hanzyong1@gmail.com>
2023-08-16 11:17:12 +0800

    add pagination

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,23 +7,26 @@
       |
       <router-link :to="{ name: 'login' }">Login</router-link>
     </nav>
+
     <h1>RESTAURANTS</h1>
+
     <div class="category-buttons">
-      <button @click="handleCategory('all')">All</button>
+      <button @click="handleCategoryClick('all')">All</button>
       <button
         v-for="category in categories"
         :key="category.id"
-        @click="handleCategory(category.attributes.name)"
+        @click="handleCategoryClick(category.attributes.name)"
       >
         {{ category.attributes.name | capitalize }}
       </button>
     </div>
+
     <div class="main">
       <template v-for="restaurant in restaurants">
         <div
           class="card"
           v-bind:key="restaurant.id"
-          @click="handleClick(restaurant.id)"
+          @click="handleRestaurantClick(restaurant.id)"
         >
           <div class="img-container">
             <img
@@ -50,6 +53,22 @@
         </div>
       </template>
     </div>
+
+    <div class="pagination-container">
+      <button class="pagination-button" @click="prevPage">&lt;</button>
+      <div class="pagination-numbers">
+        <button
+          class="pagination-number"
+          v-for="number in numberOfPages"
+          :key="number"
+          :class="{ active: currentPage == number }"
+          @click="setCurrentPage(number)"
+        >
+          {{ number }}
+        </button>
+      </div>
+      <button class="pagination-button" @click="nextPage">&gt;</button>
+    </div>
   </div>
 </template>
 
@@ -63,22 +82,57 @@ export default {
       baseUrl: process.env.VUE_APP_BASE_URL,
       restaurants: [],
       categories: [],
+      pageSize: 6,
+      numberOfPages: null,
+      currentPage: 1,
     };
   },
   methods: {
-    handleClick(value) {
+    handleRestaurantClick(value) {
       this.$router.push({ name: "restaurant", params: { id: value } });
     },
 
-    async handleCategory(category) {
+    setNumberOfPages(restaurants) {
+      this.numberOfPages = Math.ceil(restaurants.length / this.pageSize);
+    },
+
+    prevPage() {
+      this.currentPage -= 1;
+      if (this.currentPage <= 0) {
+        this.currentPage = 1;
+      }
+      this.setCurrentPage(this.currentPage);
+    },
+
+    nextPage() {
+      this.currentPage += 1;
+      if (this.currentPage >= this.numberOfPages) {
+        this.currentPage = this.numberOfPages;
+      }
+      this.setCurrentPage(this.currentPage);
+    },
+
+    setCurrentPage(page) {
+      this.currentPage = page;
+      this.getPageData();
+    },
+
+    async handleCategoryClick(category) {
       if (category == "all") {
-        this.getCardData();
+        this.currentPage = 1;
+        this.getPageData();
+        this.getAllRestaurantsData();
       } else {
         const response = await axios({
           method: "get",
           url: `${this.apiUrl}/restaurants?filters[categories][name][$eqi]=${category}&populate=*`,
         });
+
+        // populate category restaurants data
         this.restaurants = response.data.data;
+
+        // get number of pages for this category
+        this.setNumberOfPages(this.restaurants);
       }
     },
 
@@ -94,23 +148,38 @@ export default {
       }
     },
 
-    async getCardData() {
+    async getAllRestaurantsData() {
       try {
         const response = await axios({
           method: "get",
           url: `${this.apiUrl}/restaurants?populate=*`,
         });
-        this.restaurants = response.data.data;
 
-        console.log(response);
+        // get total number of pages
+        this.setNumberOfPages(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async getPageData() {
+      try {
+        const response = await axios({
+          method: "get",
+          url: `${this.apiUrl}/restaurants?pagination[page]=${this.currentPage}&pagination[pageSize]=${this.pageSize}&populate=*`,
+        });
+
+        // populate restaurants data for this page
+        this.restaurants = response.data.data;
       } catch (error) {
         console.log(error);
       }
     },
   },
   mounted() {
-    this.getCardData();
+    this.getAllRestaurantsData();
     this.getCategoryData();
+    this.getPageData();
   },
 };
 </script>
@@ -139,6 +208,10 @@ export default {
   justify-content: center;
   gap: 50px;
   padding-bottom: 50px;
+}
+
+.main > * {
+  flex: 0 0 25%;
 }
 
 nav {
@@ -189,5 +262,33 @@ img {
 
 .name {
   font-weight: bold;
+}
+
+.pagination-container {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  justify-content: center;
+  margin: 10px;
+}
+.pagination-number,
+.pagination-button {
+  font-size: 1.1rem;
+  background-color: transparent;
+  border: none;
+  margin: 0.25rem 0.25rem;
+  cursor: pointer;
+  height: 2.5rem;
+  width: 2.5rem;
+  border-radius: 0.2rem;
+}
+.pagination-number:hover,
+.pagination-button:not(.disabled):hover {
+  background: #bbb;
+}
+.pagination-number.active {
+  color: #fff;
+  background: #0085b6;
 }
 </style>
